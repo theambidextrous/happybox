fa434f0293f588421427e52f8342c394253a6e83
Soft Bucket IO <softbucket.io@gmail.com>
2021-02-26 10:41:24 +0300

    pos sales report pg

--- app/Http/Controllers/api/v0/user/UserController.php
+++ app/Http/Controllers/api/v0/user/UserController.php
@@ -728,4 +728,13 @@ class UserController extends Controller
     {
         //
     }
+    public function createCode($length = 20) {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
